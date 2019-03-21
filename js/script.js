9ebb4c5f163eef676faf489c6f27991f13fc0016
queenvsking101 <elisaholder@whsat.org>
2019-03-21 22:21:06 +0000

    first day but almost done

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var searchInput = $("input").val();
+  $.ajax({
+url: "https://api.giphy.com/v1/gifs/search?q="+searchInput+"&rating=pg&api_key=dc6zaTOxFJmzC" ,
+method: 'GET',
+success:function(response) {
+    console.log(response);
+$("#mine").html("<img src='"+response.data[0].images.fixed_width.url+"'/>");
+  }
+
+});
+
+
+
 });
 
